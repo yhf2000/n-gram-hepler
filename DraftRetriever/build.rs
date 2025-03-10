efe790f9ca54c5110661117368ab623df236eec7
Tinghong Li <2065037421@qq.com>
2025-03-10 09:43:05 +0000

    增加REST的SA构建目录

---
+++ DraftRetriever/build.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("cargo:rerun-if-changed=libsais.c");
+
+    let src = [
+        "src/libsais/libsais.c",
+    ];
+    let mut builder = cc::Build::new();
+    let build = builder
+        .files(src.iter());
+//         .flag("-fopenmp");
+    build.compile("libsais");
+}
